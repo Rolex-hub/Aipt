0d3274bad57b9b49c0348270f9d5501c3d3d06c0
Rolex-hub <164478295+Rolex-hub@users.noreply.github.com>
2024-03-23 16:03:21 +0700

    Update Api.lua

--- Api.lua
+++ Api.lua
@@ -1,71 +1,71 @@
 return {
-    print,
-    warn,
-    hookfunction,
-    setmetatable,
-    setclipboard, 
-    pcall,
-    pairs,
-    math.random,
-    newcclosure,
-    error, 
-    getnamecallmethod, 
-    string.format, 
-    string.gsub, 
-    string.match, 
-    appendfile, 
-    type, 
-    xpcall, 
-    assert, 
-    wait,
-    spawn, 
-    getrawmetatable, 
-    old, 
-    task.wait, 
-    require,
-    Vector2.new, 
-    CFrame.new,
-    UDim2.new, 
-    unpack,
-    task.spawn,
-    table.insert, 
-    tonumber, 
-    tostring,
-    os.date, 
-    sethiddenproperty, 
-    Instance.new, 
-    Color3.fromRGB, 
-    getupvalues, 
-    debug.getupvalue, 
-    math.floor,
-    loadstring,
-    request, 
-    encode, 
-    cancel,
-    tick, 
-    typeof, 
-    new,
-    getconnections,
-    makefolder,
-    isfolder, 
-    isfile, 
-    readfile, 
-    delfile,
-    listfiles, 
-    loadfile,
-    appendfile,
-    writefile, 
-    Vector3.new, 
-    coroutine.wrap,
-    fireclickdetector,
-    fireproximityprompt,
-    table.concat, 
-    clonefunction, 
-    true,
-    false,
-    CFrame,
-    _G,
-    nil,
-    CFrame.Angles,
-    math.random,
+    print, --1
+    warn, --2
+    hookfunction, --3
+    setmetatable, --4
+    setclipboard, --5
+    pcall, --6
+    pairs, --7
+    math.random, --8
+    newcclosure, --9
+    error, --10
+    getnamecallmethod, --11
+    string.format, --12
+    string.gsub, --13
+    string.match, --14
+    appendfile, --15
+    type, --16
+    xpcall, --17
+    assert, --18
+    wait, --19
+    spawn, --20
+    getrawmetatable, --21
+    old, --22
+    task.wait, --23
+    require, --24
+    Vector2.new, --25
+    CFrame.new, --26
+    UDim2.new, --27
+    unpack, --28
+    task.spawn, --29
+    table.insert, --30
+    tonumber, --31
+    tostring, --32
+    os.date, --33
+    sethiddenproperty, --34
+    Instance.new, --35
+    Color3.fromRGB, --36
+    getupvalues, --37
+    debug.getupvalue, --38
+    math.floor, --39
+    loadstring, --40
+    request, --41
+    encode, --42
+    cancel, --43
+    tick, --44
+    typeof, --45
+    new, --46
+    getconnections, --47
+    makefolder, --48
+    isfolder, --49
+    isfile, --50
+    readfile, --51
+    delfile, --52
+    listfiles, --53
+    loadfile, --54
+    appendfile, --55
+    writefile, --56
+    Vector3.new, --57
+    coroutine.wrap, --58
+    fireclickdetector, --59
+    fireproximityprompt, --60
+    table.concat, --61
+    clonefunction, --62
+    true, -- 63
+    false, --64
+    CFrame, -- 65
+    _G,--66
+    nil,--67
+    CFrame.Angles, -- 68
+    math.random, -- 69
 }
